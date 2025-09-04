98cce4a998add793b8d5b24472c0a282bb0b53eb
Pedro <pedromendes7182@gmail.com>
2025-09-04 01:13:08 -0300

    refactor(Medico): simplifica a tabela para usar apenas dados e paginação do backend

--- src/app/pages/doctors/components/doctor-table/doctor-table.component.ts
+++ src/app/pages/doctors/components/doctor-table/doctor-table.component.ts
@@ -1,24 +1,10 @@
-import {
-  AfterViewInit,
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  OnDestroy,
-  Output,
-  SimpleChanges,
-  ViewChild,
-} from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { CommonModule } from '@angular/common';
-import { Subscription } from 'rxjs';
-import { CustomPaginator } from './custom-paginator';
 import { DoctorTable } from '../../doctor.models';
 import { PageEvent } from '@angular/material/paginator';
 
@@ -36,11 +22,8 @@ export type MedicoModel = DoctorTable;
     MatPaginatorModule,
   ],
   templateUrl: './doctor-table.component.html',
-  providers: [{ provide: MatPaginatorIntl, useClass: CustomPaginator }],
 })
-export class DoctorTableComponent
-  implements AfterViewInit, OnChanges, OnDestroy
-{
+export class DoctorTableComponent {
   @Input() medicos: MedicoModel[] = [];
 
   @Input() totalElements: number = 0;
@@ -51,8 +34,6 @@ export class DoctorTableComponent
   @Output() onConfirmDelete = new EventEmitter<MedicoModel>();
   @Output() onRequestUpdate = new EventEmitter<MedicoModel>();
 
-  dataSource!: MatTableDataSource<MedicoModel>;
-
   displayedColumns: string[] = [
     'id',
     'nome',
@@ -62,24 +43,6 @@ export class DoctorTableComponent
     'telefone',
     'acoes',
   ];
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  private subs?: Subscription;
-
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['medicos'] && this.medicos) {
-      this.dataSource = new MatTableDataSource(this.medicos);
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.subs?.unsubscribe();
-  }
 
   delete(medico: MedicoModel) {
     this.onConfirmDelete.emit(medico);
